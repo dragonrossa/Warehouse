4f579a07287b493a47bfe1ecff1bf8b4ba7083e5
Rosana Đuga <r.duga@concepts.hr>
2020-08-13 16:16:06 +0200

    Warehouse for notebook - CRUD - Entity Framework

---
+++ Warehouse/Controllers/LaptopController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Warehouse.Models;
+
+namespace Warehouse.Controllers
+{
+    public class LaptopController : Controller
+    {
+        private ApplicationDbContext _db = new ApplicationDbContext();
+        // GET: MasterData
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        //GET: MasterData/Create
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        //POST: MasterData/Create
+        [HttpPost]
+        public ActionResult Create(LaptopModels laptop)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.LaptopModels.Add(laptop);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(laptop);
+        }
+
+        //GET: MasterData/List
+        public ActionResult List()
+        {
+            List<LaptopModels> ListLaptop = (from k in _db.LaptopModels select k).ToList();
+            return View(ListLaptop);
+        }
+
+        //GET: MasterData/EditList
+        public ActionResult EditList()
+        {
+            List<LaptopModels> Laptop = (from k in _db.LaptopModels select k).ToList();
+            return View(Laptop);
+        }
+
+        ////GET: MasterData/DeleteList
+        public ActionResult DeleteList()
+        {
+            List<LaptopModels> Laptop = (from k in _db.LaptopModels select k).ToList();
+            return View(Laptop);
+        }
+
+        //GET: MasterData/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LaptopModels laptop = _db.LaptopModels.Find(id);
+
+            if (laptop == null)
+            {
+                return HttpNotFound();
+            }
+
+            //List<MasterData> ListMaster = (from k in _db.LaptopModels select k).ToList();
+            return View(laptop);
+        }
+
+        //POST: MasterData/Edit/5
+        [HttpPost]
+        public ActionResult Edit(LaptopModels laptop)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(laptop).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(laptop);
+        }
+
+        //GET: MasterData/Delete/5
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LaptopModels laptop = _db.LaptopModels.Find(id);
+
+            if (laptop == null)
+            {
+                return HttpNotFound();
+            }
+            return View(laptop);
+        }
+        //POST: MasterData/Delete/5
+        [HttpPost, ActionName("Delete")]
+        public ActionResult Delete(int id)
+        {
+            LaptopModels laptop = _db.LaptopModels.Find(id);
+            _db.LaptopModels.Remove(laptop);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //GET: MasterData/Details/1
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LaptopModels laptop = _db.LaptopModels.Find(id);
+
+            if (laptop == null)
+            {
+                return HttpNotFound();
+            }
+            return View(laptop);
+        }
+
+    }
+}
---
+++ Warehouse/Models/LaptopModels.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Warehouse.Models
+{
+    public class LaptopModels
+    {
+        //ID
+        [Key]
+        public int ID { get; set; }
+        //Name
+        [Required]
+        [StringLength(100, ErrorMessage = "Name cant have more than 100 letters")]
+        public string Name { get; set; }
+        //Description
+        [Required]
+        [Column(TypeName = "text")]
+        public string Description { get; set; }
+        //Quantity
+        [Required]
+        public int Quantity { get; set; }
+        //Manufacturer
+        [StringLength(100, ErrorMessage = "Manufacturer cant have more than 100 letters")]
+        public string Manufacturer { get; set; }
+        //SN - Serial Number
+        [Column(TypeName = "text")]
+        public string SN { get; set; }
+        //OS - Operating System
+        [StringLength(50, ErrorMessage = "OS cant have more than 50 letters")]
+        public string OS { get; set; }
+        //Price
+        public int Price { get; set; }
+        //UserID
+        public int? UserID { get; set; }
+        //PlaceID
+        public int? PlaceID { get; set; }
+
+    }
+}
