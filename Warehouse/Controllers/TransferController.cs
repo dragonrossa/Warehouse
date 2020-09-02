12e12e70bf8b191daf5b9f7f935ca51e8de40e87
Rosana Đuga <r.duga@concepts.hr>
2020-09-02 20:39:50 +0200

    Create new Transfer with defined options

--- Warehouse/Controllers/TransferController.cs
+++ Warehouse/Controllers/TransferController.cs
@@ -68,20 +68,58 @@ namespace Warehouse.Controllers
                 Value = u.ID.ToString()
             }).ToList();
 
-            ViewData["LaptopName"] = _db.LaptopModels.ToList().Select(u => new SelectListItem
+            ViewData["LaptopName"] = _db.LaptopModels.Where(u => u.Quantity != 0).ToList().Select(u => new SelectListItem
             {
                 Text = u.Name,
                 Value = u.ID.ToString()
             }).ToList();
 
-
+           // var newList = ListLaptop.Where(i => i.Quantity != 0).ToList();
             //int id = ViewBag.LaptopName[2].Value;
-   
+
             //ViewBag.quantity = (from k in _db.LaptopModels 
             //                       where k.ID==k.ID
             //                    select k.Quantity).First();
 
             return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult Create(FormCollection form, TransferModels transfer)
+        {
+            
+            int storeID = Convert.ToInt32(form["StoreName"].ToString());
+            int LaptopID = Convert.ToInt32(form["LaptopName"].ToString());
+            int LaptopQuantity = Convert.ToInt32(form["LaptopQuantity"].ToString());
+
+            var possibleCount = (from k in _db.LaptopModels where k.ID == LaptopID select k.Quantity).First();
+
+            if (possibleCount > 0)
+            {
+                if (LaptopQuantity > 0)
+                {
+                    var laptop = (from k in _db.LaptopModels where k.ID == LaptopID select k.Name).First();
+                    transfer.StoreID = storeID;
+                    transfer.LaptopID = LaptopID;
+                    transfer.LaptopName = laptop;
+                    transfer.LaptopQuantity = LaptopQuantity;
+                    transfer.Date = DateTime.Now;// add if any field you want insert
+                    _db.TransferModels.Add(transfer);
+                    var laptopFind = (from k in _db.LaptopModels where k.ID == transfer.LaptopID select k).First(); //select laptop
+                    laptopFind.Quantity = laptopFind.Quantity - LaptopQuantity;  //put quantity to 0
+                    var storeFind = (from s in _db.StoreModels where s.ID == storeID select s).First(); //select store, change Qop
+                    storeFind.QoP = storeFind.QoP - LaptopQuantity; // reduce QoP for quantity number
+                    _db.SaveChanges();
+                }
+            }
+
+            else
+            {
+                return RedirectToAction("Index", "Laptop", new { });
+            }
+
+            return RedirectToAction("Index", "Transfer", new { });
         }
 
        
