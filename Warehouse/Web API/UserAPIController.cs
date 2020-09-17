38215f1a011d2587a4f0f77c09f589df0b6dee64
Rosana Đuga <r.duga@concepts.hr>
2020-09-17 18:38:18 +0200

    User web API

--- Warehouse/Web API/UserAPIController.cs
+++ Warehouse/Web API/UserAPIController.cs
@@ -35,6 +35,178 @@ namespace Warehouse.Controllers
             return Ok(user);
         }
 
+        //Example:
+        //api/logAPI/name/Rosana
+        [Route("name/{name}")]
+        public HttpResponseMessage GetUserByName([FromUri] UserModels user)
+        {
+
+            string name = user.Name;
+            if (name == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Not found");
+            }
+
+            List<UserModels> users = (from k in _db.UserModels where k.Name == name select k).ToList();
+
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, users);
+
+            return response;
+        }
+
+        //Example:
+        //api/logAPI/lastname/Đuga
+        [Route("lastname/{lastname}")]
+        public HttpResponseMessage GetUserByLastName([FromUri] UserModels user)
+        {
+
+            string lastname = user.LastName;
+            if (lastname == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Not found");
+            }
+
+            List<UserModels> users = (from k in _db.UserModels where k.LastName == lastname select k).ToList();
+
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, users);
+
+            return response;
+        }
+
+        //Example:
+        //api/logAPI/address/Test
+        [Route("address/{address}")]
+        public HttpResponseMessage GetUserByAddress([FromUri] UserModels user)
+        {
+
+            string address = user.Address;
+            if (address == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Not found");
+            }
+
+            List<UserModels> users = (from k in _db.UserModels where k.Address == address select k).ToList();
+
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, users);
+
+            return response;
+        }
+
+        //Example:
+        //api/logAPI/zipcode/10000
+        [Route("zipcode/{zipcode}")]
+        public HttpResponseMessage GetUserByZipCode([FromUri] UserModels user)
+        {
+
+            string zipcode = user.ZipCode;
+            if (zipcode == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Not found");
+            }
+
+            List<UserModels> users = (from k in _db.UserModels where k.ZipCode == zipcode select k).ToList();
+
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, users);
+
+            return response;
+        }
+
+        //Example:
+        //api/logAPI/country/Croatia
+        [Route("country/{country}")]
+        public HttpResponseMessage GetUserByCountry([FromUri] UserModels user)
+        {
+
+            string country = user.Country;
+            if (country == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Not found");
+            }
+
+            List<UserModels> users = (from k in _db.UserModels where k.Country == country select k).ToList();
+
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, users);
+
+            return response;
+        }
+
+        //Example:
+        //api/logAPI/mail/rodjuga@gmail.com/
+        [Route("mail/{mail}")]
+        public HttpResponseMessage GetUserByEmail([FromUri] UserModels user)
+        {
+
+            string mail = user.Mail;
+            if (mail == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Not found");
+            }
+
+            List<UserModels> users = (from k in _db.UserModels where k.Mail == mail select k).ToList();
+
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, users);
+
+            return response;
+        }
+
+        //Example:
+        //api/logAPI/telephone/01010101
+        [Route("telephone/{telephone}")]
+        public HttpResponseMessage GetUserByTelephone([FromUri] UserModels user)
+        {
+
+            string telephone = user.Telephone;
+            if (telephone == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Not found");
+            }
+
+            List<UserModels> users = (from k in _db.UserModels where k.Telephone == telephone select k).ToList();
+
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, users);
+
+            return response;
+        }
+
+        //Example:
+        //api/logAPI/username/rodjuga@gmail.com/
+        [Route("username/{username}")]
+        public HttpResponseMessage GetUserByUserName([FromUri] UserModels user)
+        {
+
+            string username = user.UserName;
+            if (username == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Not found");
+            }
+
+            List<UserModels> users = (from k in _db.UserModels where k.UserName == username select k).ToList();
+
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, users);
+
+            return response;
+        }
+
+        //Example:
+        //api/logAPI/hometown/Zagreb
+        [Route("hometown/{hometown}")]
+        public HttpResponseMessage GetUserByHometown([FromUri] UserModels user)
+        {
+
+            string hometown = user.Hometown;
+            if (hometown == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Not found");
+            }
+
+            List<UserModels> users = (from k in _db.UserModels where k.Hometown == hometown select k).ToList();
+
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, users);
+
+            return response;
+        }
+
+
 
     }
 }
