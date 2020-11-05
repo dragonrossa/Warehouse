6dc16a39d7a35e2917c9f1faddf9564918e06d47
Rosana Đuga <r.duga@concepts.hr>
2020-11-05 12:02:59 +0100

    Implementation of Interface for LogModels and functions for LogController

--- Warehouse/Controllers/LogController.cs
+++ Warehouse/Controllers/LogController.cs
@@ -13,65 +13,70 @@ namespace Warehouse.Controllers
         private ApplicationDbContext _db = new ApplicationDbContext();
         // GET: Log
 
-        public class UserNotFoundException : Exception
+
+        //Get user info
+
+        public string Username()
         {
-            public UserNotFoundException() : base() { }
-            public UserNotFoundException(string message) : base(message) { }
-            public UserNotFoundException(string message, Exception innerException)
-                : base(message, innerException) { }
+            return User.Identity.GetUserName();
         }
 
 
- 
 
-    public ActionResult Index()
+        public LogModels logsList()
+        {
+            // Logs for Laptop
+
+            List<LogModels> log = (from k in _db.LogModels where k.Type == "0" select k)
+                                             .OrderBy(x => x.ID)
+                                             .ToList();
+            //Logs for Store
+            List<LogModels> log1 = (from k in _db.LogModels where k.Type == "1" select k)
+                                             .OrderBy(x => x.ID)
+                                             .ToList();
+            //Logs for Transfer
+            List<LogModels> log2 = (from k in _db.LogModels where k.Type == "2" select k)
+                                            .OrderBy(x => x.ID)
+                                            .ToList();
+            //Logs for Search
+            List<LogModels> log3 = (from k in _db.LogModels where k.Type == "3" select k)
+                                        .OrderBy(x => x.ID)
+                                        .ToList();
+            //Logs for Search - not found
+            List<LogModels> log4 = (from k in _db.LogModels where k.Type == "4" select k)
+                                       .OrderBy(x => x.ID)
+                                       .ToList();
+            //Logs for New user
+            List<LogModels> log5 = (from k in _db.LogModels where k.Type == "5" select k)
+                                      .OrderBy(x => x.ID)
+                                      .ToList();
+
+            //Logs for Admin console
+            List<LogModels> log6 = (from k in _db.LogModels where k.Type == "6" select k)
+                                      .OrderBy(x => x.ID)
+                                      .ToList();
+
+            //Logs for Access console
+            List<LogModels> log7 = (from k in _db.LogModels where k.Type == "7" select k)
+                                      .OrderBy(x => x.ID)
+                                      .ToList();
+
+            return new LogModels() { log = log, log1 = log1, log2 = log2, log3 = log3, log4 = log4, log5 = log5, log6 = log6, log7 = log7 };
+        }
+
+
+
+        public ActionResult Index()
         {
 
-            var user = User.Identity.GetUserName();
-
-
+         
             try
             {
 
-                // Logs for Laptop
- 
-             List < LogModels > log = (from k in _db.LogModels where k.Type == "0" select k)
-                                              .OrderBy(x => x.ID)
-                                              .ToList();
-                 //Logs for Store
-                 List < LogModels > log1 = (from k in _db.LogModels where k.Type == "1" select k)
-                                                  .OrderBy(x => x.ID)
-                                                  .ToList();
-                //Logs for Transfer
-                List<LogModels> log2 = (from k in _db.LogModels where k.Type == "2" select k)
-                                                .OrderBy(x => x.ID)
-                                                .ToList();
-                //Logs for Search
-                List<LogModels> log3 = (from k in _db.LogModels where k.Type == "3" select k)
-                                            .OrderBy(x => x.ID)
-                                            .ToList();
-                //Logs for Search - not found
-                List<LogModels> log4 = (from k in _db.LogModels where k.Type == "4" select k)
-                                           .OrderBy(x => x.ID)
-                                           .ToList();
-                //Logs for New user
-                List<LogModels> log5 = (from k in _db.LogModels where k.Type == "5" select k)
-                                          .OrderBy(x => x.ID)
-                                          .ToList();
 
-                //Logs for Admin console
-                List<LogModels> log6 = (from k in _db.LogModels where k.Type == "6" select k)
-                                          .OrderBy(x => x.ID)
-                                          .ToList();
+                return View(logsList());
 
-                //Logs for Access console
-                List<LogModels> log7 = (from k in _db.LogModels where k.Type == "7" select k)
-                                          .OrderBy(x => x.ID)
-                                          .ToList();
-
-                return View(new LogModels() { log = log, log1 = log1, log2 = log2, log3 = log3, log4 = log4, log5 = log5, log6 = log6, log7 = log7 });
-
-
+  
 
             }
             catch (Exception e)
--- Warehouse/Models/LogModels.cs
+++ Warehouse/Models/LogModels.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
-using System.Web;
+using System.Web.UI.WebControls;
+using Warehouse.Helpers;
+
 
 namespace Warehouse.Models
 {
-    public class LogModels
+    public class LogModels:IElement<LogModels>
     {
+        private ApplicationDbContext _db = new ApplicationDbContext();
+
         //ID
         [Key]
         public int ID { get; set; }
@@ -22,13 +26,56 @@ namespace Warehouse.Models
         public DateTime? Date { get; set; }
 
         //Custom models needed for Index for Type 0, 1, 2, 3, 4, 5, 6, 7
+        [NotMapped]
         public List<LogModels> log { get; set; }
+        [NotMapped]
         public List<LogModels> log1 { get; set; }
+        [NotMapped]
         public List<LogModels> log2 { get; set; }
+        [NotMapped]
         public List<LogModels> log3 { get; set; }
+        [NotMapped]
         public List<LogModels> log4 { get; set; }
+        [NotMapped]
         public List<LogModels> log5 { get; set; }
+        [NotMapped]
         public List<LogModels> log6 { get; set; }
+        [NotMapped]
         public List<LogModels> log7 { get; set; }
+
+        //All logs
+        [NotMapped]
+        public List<LogModels> Child
+        {
+            get
+            {
+                return (from t in _db.LogModels select t).ToList();
+            }
+        }
+
+        //Logs ordered by Asc
+
+        [NotMapped]
+        public List<LogModels> Ascending
+        {
+            get
+            {
+                return _db.LogModels.OrderBy(x => x.ID).ToList();
+            }
+        }
+
+
+        //Logs ordered by Desc
+
+        [NotMapped]
+        public List<LogModels> Descending
+        {
+            get
+            {
+                return _db.LogModels.OrderByDescending(x => x.ID).ToList();
+            }
+        }
+
+      
     }
 }
