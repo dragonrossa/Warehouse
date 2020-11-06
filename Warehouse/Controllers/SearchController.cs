d7415447c0b88280974e32ca8a0251d996be971f
Rosana Đuga <r.duga@concepts.hr>
2020-11-06 14:05:47 +0100

    Created functions for SearchControllers ActionResults

--- Warehouse/Controllers/SearchController.cs
+++ Warehouse/Controllers/SearchController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Dynamic;
 using System.Linq;
 using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using System.Web;
 using System.Web.Mvc;
 using Warehouse.Helpers;
@@ -13,24 +14,36 @@ namespace Warehouse.Controllers
 {
     public class SearchController : Controller
     {
+        //Call database
         private ApplicationDbContext _db = new ApplicationDbContext();
+
+        //Create class object
         SearchIndex search = new SearchIndex();
+
+        //Initialize new Search List
         List<SearchIndex> index = new List<SearchIndex>();
 
+
+        //If needed, user info
         public string userInfo()
         {
             var user = User.Identity.GetUserName();
             return user;
         }
 
-
-
+        
         public List<LaptopModels> findSearch(SearchIndex search)
         {
 
-            int QuantityOfAllProducts = (from k in _db.LaptopModels where k.Manufacturer == search.Name select k).Count();
-            search.QuantityOfAllProducts = (from k in _db.LaptopModels where k.OS == search.Name select k).Count();
-            List<LaptopModels> laptops = (from k in _db.LaptopModels where k.Manufacturer == search.Name select k).ToList();
+            int QuantityOfAllProducts = (from k in _db.LaptopModels
+                                         where k.Manufacturer == search.Name 
+                                         select k).Count();
+            search.QuantityOfAllProducts = (from k in _db.LaptopModels 
+                                            where k.OS == search.Name 
+                                            select k).Count();
+            List<LaptopModels> laptops = (from k in _db.LaptopModels 
+                                          where k.Manufacturer == search.Name 
+                                          select k).ToList();
             return laptops;
 
         }
@@ -38,10 +51,16 @@ namespace Warehouse.Controllers
         public List<LaptopModels> findSearchOS(SearchIndex search)
         {
 
-          
-            int QuantityOfAllProducts = (from k in _db.LaptopModels where k.OS == search.Name select k).Count();
-            search.QuantityOfAllProducts = (from k in _db.LaptopModels where k.OS == search.Name select k).Count();
-            List<LaptopModels> laptops = (from k in _db.LaptopModels where k.OS == search.Name select k).ToList();
+
+            int QuantityOfAllProducts = (from k in _db.LaptopModels 
+                                         where k.OS == search.Name 
+                                         select k).Count();
+            search.QuantityOfAllProducts = (from k in _db.LaptopModels 
+                                            where k.OS == search.Name 
+                                            select k).Count();
+            List<LaptopModels> laptops = (from k in _db.LaptopModels 
+                                          where k.OS == search.Name 
+                                          select k).ToList();
             return laptops;
 
         }
@@ -49,55 +68,149 @@ namespace Warehouse.Controllers
         public List<StoreModels> findSearchLocation(SearchIndex search)
         {
 
-            List<StoreModels> stores = (from k in _db.StoreModels where k.Location == search.Name select k).ToList();
+            List<StoreModels> stores = (from k in _db.StoreModels 
+                                        where k.Location == search.Name 
+                                        select k).ToList();
             return stores;
 
         }
 
+
+
         public LaptopModels searchName(SearchIndex search)
         {
-            return (from k in _db.LaptopModels where k.Name == search.Name select k).FirstOrDefault();
+            return (from k in _db.LaptopModels 
+                    where k.Name == search.Name 
+                    select k).FirstOrDefault();
         }
 
+
+        //Quantity for Laptop
         public int? calculateQuantity(SearchIndex search)
         {
-            return (from k in _db.LaptopModels where k.Name == search.Name select (int?)k.Quantity).Sum();
+            return (from k in _db.LaptopModels 
+                    where k.Name == search.Name 
+                    select (int?)k.Quantity).Sum();
         }
 
+        //Quantity for Store
         public int? calculateStoreQuantity(SearchIndex search)
         {
-            return (from k in _db.StoreModels where k.Name == search.Name select (int?)k.QoP).Sum();
+            return (from k in _db.StoreModels 
+                    where k.Name == search.Name 
+                    select (int?)k.QoP).Sum();
         }
 
+        //Quantitiy for Location
         public int? calculateLocationQuantity(SearchIndex search)
         {
-            return (from k in _db.StoreModels where k.Location == search.Name select (int?)k.QoP).Sum();
+            return (from k in _db.StoreModels 
+                    where k.Location == search.Name 
+                    select (int?)k.QoP).Sum();
+        }
+
+        //Quantity for ZipCode
+        public int? calculateZipCodeQuantity(int? test)
+        {
+            return (from k in _db.StoreModels 
+                    where k.ZipCode == test 
+                    select (int?)k.QoP).Sum();
+        }
+
+        //Quantity for Transfer
+        public int? calculateTransferQuantity(SearchIndex search)
+        {
+            return (from k in _db.TransferModels 
+                    where k.LaptopName == search.Name 
+                    select (int?)k.LaptopQuantity).Sum();
+        }
+
+        //Quantity for Transfer Store Quantity
+        public int? calculateTransferStoreQuantity(int store)
+        {
+            return (from t in _db.TransferModels 
+                    where t.StoreID == store 
+                    select (int?)t.LaptopQuantity).Sum();
         }
 
         public LaptopModels searchManufacturer(SearchIndex search)
         {
-            return (from k in _db.LaptopModels where k.Manufacturer == search.Name select k).FirstOrDefault();
+            return (from k in _db.LaptopModels 
+                    where k.Manufacturer == search.Name 
+                    select k).FirstOrDefault();
         }
 
         public LaptopModels searchOS(SearchIndex search)
         {
-            return (from k in _db.LaptopModels where k.OS == search.Name select k).FirstOrDefault();
+            return (from k in _db.LaptopModels 
+                    where k.OS == search.Name 
+                    select k).FirstOrDefault();
         }
 
         public StoreModels searchStores(SearchIndex search)
         {
-            return (from k in _db.StoreModels where k.Name == search.Name select k).FirstOrDefault();
+            return (from k in _db.StoreModels 
+                    where k.Name == search.Name 
+                    select k).FirstOrDefault();
+        }
+
+        public StoreModels searchZipCode(int? test)
+        {
+            return (from k in _db.StoreModels 
+                    where k.ZipCode == test 
+                    select k).FirstOrDefault();
+        }
+
+        public TransferModels searchStore(int store)
+        {
+            return (from t in _db.TransferModels 
+                    where t.StoreID == store 
+                    select t).FirstOrDefault();
         }
 
         public List<StoreModels> storeList(SearchIndex search)
         {
-            return (from k in _db.StoreModels where k.Name == search.Name select k).ToList();
+            return (from k in _db.StoreModels 
+                    where k.Name == search.Name 
+                    select k).ToList();
+        }
+
+        public List<StoreModels> storeListByZipCode(int? test)
+        {
+            return (from k in _db.StoreModels 
+                    where k.ZipCode == test 
+                    select k).ToList();
         }
 
 
         public StoreModels searchLocation(SearchIndex search)
         {
-            return (from k in _db.StoreModels where k.Location == search.Name select k).FirstOrDefault();
+            return (from k in _db.StoreModels 
+                    where k.Location == search.Name 
+                    select k).FirstOrDefault();
+        }
+
+        public List<TransferModels> searchTransferByLaptopName(SearchIndex search)
+        {
+            return (from k in _db.TransferModels 
+                    where k.LaptopName == search.Name 
+                    select k).ToList();
+        }
+
+
+        public int storeID(SearchIndex search)
+        {
+            return (from s in _db.StoreModels
+                    where s.Name == search.Name
+                    select s.ID).SingleOrDefault();
+
+        }
+
+        public List<TransferModels> transfer(int store)
+        {
+            return (from t in _db.TransferModels
+                    where t.StoreID == store
+                    select t).ToList();
         }
 
         //Create new log
@@ -131,10 +244,7 @@ namespace Warehouse.Controllers
             return log;
         }
 
-    
 
-
-        
 
         // GET: Search
         //Search - Laptop
@@ -383,37 +493,21 @@ namespace Warehouse.Controllers
                 int? test = Convert.ToInt32(search.Name);
                 ViewBag.Name = search.Name;
                 TempData["searchName"] = ViewBag.Name;
-                ViewBag.searchName = (from k in _db.StoreModels where k.ZipCode == test select k).FirstOrDefault();
-                List<StoreModels> stores = (from k in _db.StoreModels where k.ZipCode == test select k).ToList();
-                ViewBag.quantity = (from k in _db.StoreModels where k.ZipCode == test select (int?)k.QoP).Sum();
+                ViewBag.searchName = searchZipCode(test);
+                ViewBag.quantity = calculateZipCodeQuantity(test);
 
                 if (ViewBag.searchName == null)
                 {
-                    LogModels log = new LogModels
-                    {
-                        Type = "4",
-                        Description = "There was new search in Transfer section for " + search.Name + ".",
-                        Date = DateTime.Now
-                    };
-
-                    _db.LogModels.Add(log);
-                    _db.SaveChanges();
+                    //Create new log
+                    log4();
                     return View("ResultNotExists");
                 }
                 else
                 {
 
                     //Create new log
-                    LogModels log = new LogModels
-                    {
-                        Type = "3",
-                        Description = "There was new search in Store section for " + search.Name + ".",
-                        Date = DateTime.Now
-                    };
-
-                    _db.LogModels.Add(log);
-                    _db.SaveChanges();
-                    return View("ResultStore", stores);
+                    log3();
+                    return View("ResultStore", storeListByZipCode(test));
                 }
             }
 
@@ -422,7 +516,6 @@ namespace Warehouse.Controllers
 
         public ActionResult SearchTransfer(FormCollection form)
         {
-            SearchIndex search = new SearchIndex();
          
 
             //Search for Store
@@ -432,38 +525,21 @@ namespace Warehouse.Controllers
                 search.Name = form["laptopName"];
                 ViewBag.Name = search.Name;
                 TempData["searchName"] = ViewBag.Name;
-                ViewBag.searchname = (from k in _db.TransferModels where k.LaptopName == search.Name select k).FirstOrDefault();
-                ViewBag.quantity = (from k in _db.TransferModels where k.LaptopName == search.Name select (int?)k.LaptopQuantity).Sum();
-                List<TransferModels> transfers = (from k in _db.TransferModels where k.LaptopName == search.Name select k).ToList();
-
+                ViewBag.searchname = searchTransferByLaptopName(search);
+                ViewBag.quantity = calculateTransferQuantity(search);
                 if (ViewBag.searchname == null && ViewBag.quantity == null)
                 {
-                    LogModels log = new LogModels
-                    {
-                        Type = "4",
-                        Description = "There was new search in Transfer section for " + search.Name + ".",
-                        Date = DateTime.Now
-                    };
-
-                    _db.LogModels.Add(log);
-                    _db.SaveChanges();
+                    //Create new log
+                    log4();
                     return View("ResultNotExists");
                 }
                 else
                 {
-               
-                    //Add store object to list
-                
-                    LogModels log = new LogModels
-                    {
-                        Type = "3",
-                        Description = "There was new search in Transfer section for " + search.Name + ".",
-                        Date = DateTime.Now
-                    };
 
-                    _db.LogModels.Add(log);
-                    _db.SaveChanges();
-                    return View("ResultTransfer",transfers);
+                    //Add store object to list
+
+                    log3();
+                    return View("ResultTransfer", searchTransferByLaptopName(search));
                 }
             }
          
@@ -483,59 +559,25 @@ namespace Warehouse.Controllers
 
                 //check by storeName
 
+                int store = storeID(search);
 
-                //First find storeID
-
-                int storeID = (from s in _db.StoreModels
-                               where s.Name == search.Name
-                               select s.ID).SingleOrDefault();
-
-                //then find store name
-
-            
-
-                List <TransferModels> transfers = (from t in _db.TransferModels
-                                    where t.StoreID == storeID
-                                    select t).ToList();
-
-                //write down name for store
-
-                ViewBag.searchname = (from t in _db.TransferModels
-                                      where t.StoreID == storeID
-                                      select t).FirstOrDefault();
-
-                ViewBag.quantity = (from t in _db.TransferModels
-                                    where t.StoreID == storeID
-                                    select (int?)t.LaptopQuantity).Sum();
-
+                ViewBag.searchname = searchStore(store);
+              
+                ViewBag.quantity = calculateTransferStoreQuantity(store);
+                   
 
                 if (ViewBag.searchname == null && ViewBag.quantity == null)
                 {
-                    LogModels log = new LogModels
-                    {
-                        Type = "4",
-                        Description = "There was new search in Transfer section for " + search.Name + ".",
-                        Date = DateTime.Now
-                    };
-
-                    _db.LogModels.Add(log);
-                    _db.SaveChanges();
+                    //Create new log
+                    log4();
                     return View("ResultNotExists");
                 }
                 else
                 {
 
-
-                    LogModels log = new LogModels
-                    {
-                        Type = "3",
-                        Description = "There was new search in Transfer section for " + search.Name + ".",
-                        Date = DateTime.Now
-                    };
-
-                    _db.LogModels.Add(log);
-                    _db.SaveChanges();
-                    return View("ResultTransfer", transfers);
+                    //Create new log
+                    log3();
+                    return View("ResultTransfer", transfer(store));
                 }
             }
 
