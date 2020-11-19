3880b8938cadd6f456c456ee35c7150b2a0cf7c5
Rosana Đuga <r.duga@concepts.hr>
2020-11-19 15:50:25 +0100

    Repository for LogController

---
+++ Warehouse/Repository/ILogRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Warehouse.Models;
+
+namespace Warehouse.Repository
+{
+    interface ILogRepository
+    {
+        //Get context
+        ApplicationDbContext Data(ApplicationDbContext _db);
+        //Get log list for every type
+        LogModels logsList();
+        //Save data
+        object SaveData();
+    }
+}
