87e215ed835d8a1e8626bca1dc601eac4afeb904
Rosana Đuga <r.duga@concepts.hr>
2020-11-23 13:23:04 +0100

    Repository for TaskListController

--- Warehouse/Controllers/TaskListController.cs
+++ Warehouse/Controllers/TaskListController.cs
@@ -14,223 +14,21 @@ namespace Warehouse.Controllers
 {
     public class TaskListController : Controller
     {
-        // GET: TaskList
+        //Get TaskList Repository
 
-        private ApplicationDbContext _db = new ApplicationDbContext();
+        TaskListRepository taskListRepository = new TaskListRepository();
+
+        //TaskListModels object
         TaskListModels taskListModels = new TaskListModels();
 
-        //Username of this user
 
-        public string user()
-        {
-            var user = User.Identity.GetUserName();
-            return user;
-        }
-
-        //Create task after submiting form
-        public TaskListModels createTask(TaskListModels task, System.Web.Mvc.FormCollection form)
-        {
-            task.Details = form["Details"];
-            task.User = User.Identity.GetUserName();
-            _db.TaskListModels.Add(task);
-            _db.SaveChanges();
-            return task;
-        }
-
-        //Get back list of false tasks to user
-        public List<TaskListModels> listOfFalseTasks()
-        {
-            bool status = false;
-            var list = (from l in _db.TaskListModels where l.Status == status select l).ToList();
-            return list;
-        }
-
-        //Change status after clicking on checkbox
-        public TaskListModels changeStatus(System.Web.Mvc.FormCollection form, int id)
-        {
-            var task = (from t in _db.TaskListModels where t.ID == id select t).FirstOrDefault();
-            string status = form["status"];
-            task.Status = Convert.ToBoolean(form["status"]);
-            _db.SaveChanges();
-            return task;
-        }
-
-        //Find task by ID
-        public TaskListModels findTask(int id)
-        {
-            return (from t in _db.TaskListModels where t.ID == id select t).FirstOrDefault();
-
-        }
-
-
-        //Select list for Assistant 1
-
-        public List<SelectListItem> assistant1()
-        {
-            return _db.UserModels.ToList().Select(u => new SelectListItem
-            {
-                Text = u.UserName,
-                Value = u.UserName
-            }).ToList();
-        }
-
-        //Select list for Assistant 2
-        public List<SelectListItem> assistant2()
-        {
-            return _db.UserModels.ToList().Select(u => new SelectListItem
-            {
-                Text = u.UserName,
-                Value = u.UserName
-            }).ToList();
-        }
-
-        //Select list for Assistant2
-        public List<SelectListItem> assistant3()
-        {
-            return _db.UserModels.ToList().Select(u => new SelectListItem
-            {
-                Text = u.UserName,
-                Value = u.UserName
-            }).ToList();
-        }
-
-        public object getAssistant1Info(int id)
-        {
-            if (String.IsNullOrEmpty(findTask(id).Assistant1))
-            {
-                TempData["assistant1"] = "not assigned yet";
-            }
-
-
-            return TempData["assistant1"];
-        }
-
-        public object getAssistant2Info(int id)
-        {
-            if (String.IsNullOrEmpty(findTask(id).Assistant2))
-            {
-                TempData["assistant2"] = "not assigned yet";
-            }
-            return TempData["assistant2"];
-        }
-
-        public object getAssistant3Info(int id)
-        {
-
-            if (String.IsNullOrEmpty(findTask(id).Assistant3))
-            {
-                TempData["assistant3"] = "not assigned yet";
-            }
-            return TempData["assistant3"];
-        }
-
-        public string setAssistant1(int id, string assistantID1)
-        {
-
-
-            if (!String.IsNullOrEmpty(assistantID1))
-            {
-                findTask(id).Assistant1 = assistantID1;
-                _db.SaveChanges();
-
-            }
-            else
-            {
-
-            }
-
-            return findTask(id).Assistant1;
-        }
-
-        public string setAssistant2(int id, string assistantID2)
-        {
-
-
-            if (!String.IsNullOrEmpty(assistantID2))
-            {
-                findTask(id).Assistant2 = assistantID2;
-                _db.SaveChanges();
-            }
-            else
-            {
-
-            }
-            return findTask(id).Assistant2;
-        }
-
-        public string setAssistant3(int id, string assistantID3)
-        {
-
-            if (!String.IsNullOrEmpty(assistantID3))
-            {
-                findTask(id).Assistant3 = assistantID3;
-                _db.SaveChanges();
-            }
-            else
-            {
-
-            }
-            return findTask(id).Assistant2;
-        }
-
-        //Upload file in Details section
-
-        public byte[] uploadFile(int id, HttpPostedFileBase postedFile)
-        {
-            byte[] bytes;
-
-            using (BinaryReader br = new BinaryReader(postedFile.InputStream))
-            {
-                bytes = br.ReadBytes(postedFile.ContentLength);
-
-            }
-
-            var str = System.Text.Encoding.Default.GetString(bytes);
-
-            findTask(id).UploadName = postedFile.FileName;
-            findTask(id).ContentType = postedFile.ContentType;
-            findTask(id).Data = bytes;
-
-            _db.SaveChanges();
-
-            return bytes;
-
-
-        }
-
-
-        //Download previously uploaded file
-
-        public FileContentResult downloadFile(int? FileId)
-        {
-            TaskListModels taskList = new TaskListModels();
-            var file = _db.TaskListModels.ToList().Find(p => p.ID == FileId);
-            return File(file.Data, file.ContentType, file.UploadName);
-        }
-
-        //Delete uploaded file
-
-        public TaskListModels deleteFile(int? FileId2)
-        {
-
-            var file = (from p in _db.TaskListModels where p.ID == FileId2 select p).FirstOrDefault();
-            file.UploadName = null;
-            file.ContentType = null;
-            file.Data = null;
-            _db.SaveChanges();
-            return file;
-        }
-
-
-
-
+        // GET: TaskList
         public ActionResult Index()
         {
 
-
             try
             {
-                ViewBag.user = user();
+                ViewBag.user = taskListRepository.user(User.Identity.GetUserName());
                 return View();
             }
             catch (Exception e)
@@ -263,7 +61,7 @@ namespace Warehouse.Controllers
         public ActionResult Create(TaskListModels task, System.Web.Mvc.FormCollection form)
         {
 
-            createTask(task, form);
+            taskListRepository.createTask(task, form, User.Identity.GetUserName());
             return RedirectToAction("MyList");
         }
 
@@ -272,7 +70,7 @@ namespace Warehouse.Controllers
             try
             {
               
-                return View(listOfFalseTasks());
+                return View(taskListRepository.listOfFalseTasks());
             }
             catch (Exception)
             {
@@ -294,7 +92,7 @@ namespace Warehouse.Controllers
             {
                 if (ModelState.IsValid)
                 {
-                    changeStatus(form, id);
+                    taskListRepository.changeStatus(form, id);
 
                     return RedirectToAction("Index");
                 }
@@ -321,25 +119,25 @@ namespace Warehouse.Controllers
             try
             {
          
-                TempData["assistant1"] = findTask(id).Assistant1;
+                TempData["assistant1"] = taskListRepository.findTask(id).Assistant1;
 
-                TempData["assistant2"] = findTask(id).Assistant2;
+                TempData["assistant2"] = taskListRepository.findTask(id).Assistant2;
 
-                TempData["assistant3"] = findTask(id).Assistant3;
+                TempData["assistant3"] = taskListRepository.findTask(id).Assistant3;
 
-                TempData["id"] = findTask(id).ID;
+                TempData["id"] = taskListRepository.findTask(id).ID;
 
 
                 //Get info if there are assistants checked from before
-                getAssistant1Info(id);
-                getAssistant2Info(id);
-                getAssistant3Info(id);
+                taskListRepository.getAssistant1Info(id);
+                taskListRepository.getAssistant2Info(id);
+                taskListRepository.getAssistant3Info(id);
 
-                ViewData["assistant1"] = assistant1();
-                ViewData["assistant2"] = assistant2();
-                ViewData["assistant3"] = assistant3();
+                ViewData["assistant1"] = taskListRepository.assistant1();
+                ViewData["assistant2"] = taskListRepository.assistant2();
+                ViewData["assistant3"] = taskListRepository.assistant3();
                 
-                return View(findTask(id));
+                return View(taskListRepository.findTask(id));
 
             }
             catch (Exception)
@@ -356,11 +154,7 @@ namespace Warehouse.Controllers
 
         public ActionResult Details(System.Web.Mvc.FormCollection form, HttpPostedFileBase postedFile)
         {
-            //Get Assistants from form
-            string assistantID1 = form["assistant1"].ToString();
-            string assistantID2 = form["assistant2"].ToString();
-            string assistantID3 = form["assistant3"].ToString();
-
+            
             //Send to ViewBag
             int id = Convert.ToInt32(form["ID"]);
             ViewBag.id = id;
@@ -370,21 +164,21 @@ namespace Warehouse.Controllers
 
                 //Set assistants to user
 
-                setAssistant1(id, assistantID1);
-                setAssistant2(id, assistantID2);
-                setAssistant3(id, assistantID3);
+                taskListRepository.setAssistant1(id, form["assistant1"].ToString());
+                taskListRepository.setAssistant2(id, form["assistant2"].ToString());
+                taskListRepository.setAssistant3(id, form["assistant3"].ToString());
 
             }
             else
             {
 
                 //Set assistants
-                setAssistant1(id, assistantID1);
-                setAssistant2(id, assistantID2);
-                setAssistant3(id, assistantID3);
+                taskListRepository.setAssistant1(id, form["assistant1"].ToString());
+                taskListRepository.setAssistant2(id, form["assistant2"].ToString());
+                taskListRepository.setAssistant3(id, form["assistant3"].ToString());
 
                 //Save uploaded file
-                uploadFile(id, postedFile);
+                taskListRepository.uploadFile(id, postedFile);
             }
 
 
@@ -397,15 +191,15 @@ namespace Warehouse.Controllers
         public FileResult DownloadFile(int? FileId)
         {
 
-           return downloadFile(FileId);
+           return taskListRepository.downloadFile(FileId);
         }
 
         [HttpPost]
         [HandleError]
         public ActionResult DeleteFile(int? FileId2)
         {
-           
-            deleteFile(FileId2);
+
+            taskListRepository.deleteFile(FileId2);
             return RedirectToAction("MyList");
         }
 
---
+++ Warehouse/Repository/TaskListRepository.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Warehouse.Models;
+
+namespace Warehouse.Repository
+{
+    public class TaskListRepository: Controller, ITaskListRepository
+    {
+        // GET: TaskList
+
+         private ApplicationDbContext _db = new ApplicationDbContext();
+        TaskListModels taskListModels = new TaskListModels();
+
+        //Username of this user
+
+        public string user(string username)
+        {
+            var user = username;
+            return user;
+        }
+
+        //Create task after submiting form
+        public TaskListModels createTask(TaskListModels task, System.Web.Mvc.FormCollection form, string username)
+        {
+            task.Details = form["Details"];
+            task.User = user(username);
+            _db.TaskListModels.Add(task);
+            _db.SaveChanges();
+            return task;
+        }
+
+        //Get back list of false tasks to user
+        public List<TaskListModels> listOfFalseTasks()
+        {
+            bool status = false;
+            var list = (from l in _db.TaskListModels where l.Status == status select l).ToList();
+            return list;
+        }
+
+        //Change status after clicking on checkbox
+        public TaskListModels changeStatus(System.Web.Mvc.FormCollection form, int id)
+        {
+            var task = (from t in _db.TaskListModels where t.ID == id select t).FirstOrDefault();
+            string status = form["status"];
+            task.Status = Convert.ToBoolean(form["status"]);
+            _db.SaveChanges();
+            return task;
+        }
+
+        //Find task by ID
+        public TaskListModels findTask(int id)
+        {
+            return (from t in _db.TaskListModels where t.ID == id select t).FirstOrDefault();
+
+        }
+
+
+        //Select list for Assistant 1
+
+        public List<SelectListItem> assistant1()
+        {
+            return _db.UserModels.ToList().Select(u => new SelectListItem
+            {
+                Text = u.UserName,
+                Value = u.UserName
+            }).ToList();
+        }
+
+        //Select list for Assistant 2
+        public List<SelectListItem> assistant2()
+        {
+            return _db.UserModels.ToList().Select(u => new SelectListItem
+            {
+                Text = u.UserName,
+                Value = u.UserName
+            }).ToList();
+        }
+
+        //Select list for Assistant2
+        public List<SelectListItem> assistant3()
+        {
+            return _db.UserModels.ToList().Select(u => new SelectListItem
+            {
+                Text = u.UserName,
+                Value = u.UserName
+            }).ToList();
+        }
+
+        public object getAssistant1Info(int id)
+        {
+            if (String.IsNullOrEmpty(findTask(id).Assistant1))
+            {
+                TempData["assistant1"] = "not assigned yet";
+            }
+
+
+            return TempData["assistant1"];
+        }
+
+        public object getAssistant2Info(int id)
+        {
+            if (String.IsNullOrEmpty(findTask(id).Assistant2))
+            {
+                TempData["assistant2"] = "not assigned yet";
+            }
+            return TempData["assistant2"];
+        }
+
+        public object getAssistant3Info(int id)
+        {
+
+            if (String.IsNullOrEmpty(findTask(id).Assistant3))
+            {
+                TempData["assistant3"] = "not assigned yet";
+            }
+            return TempData["assistant3"];
+        }
+
+        public string setAssistant1(int id, string assistantID1)
+        {
+
+
+            if (!String.IsNullOrEmpty(assistantID1))
+            {
+                findTask(id).Assistant1 = assistantID1;
+                _db.SaveChanges();
+
+            }
+            else
+            {
+
+            }
+
+            return findTask(id).Assistant1;
+        }
+
+        public string setAssistant2(int id, string assistantID2)
+        {
+
+
+            if (!String.IsNullOrEmpty(assistantID2))
+            {
+                findTask(id).Assistant2 = assistantID2;
+                _db.SaveChanges();
+            }
+            else
+            {
+
+            }
+            return findTask(id).Assistant2;
+        }
+
+        public string setAssistant3(int id, string assistantID3)
+        {
+
+            if (!String.IsNullOrEmpty(assistantID3))
+            {
+                findTask(id).Assistant3 = assistantID3;
+                _db.SaveChanges();
+            }
+            else
+            {
+
+            }
+            return findTask(id).Assistant2;
+        }
+
+        //Upload file in Details section
+
+        public byte[] uploadFile(int id, HttpPostedFileBase postedFile)
+        {
+            byte[] bytes;
+
+            using (BinaryReader br = new BinaryReader(postedFile.InputStream))
+            {
+                bytes = br.ReadBytes(postedFile.ContentLength);
+
+            }
+
+            var str = System.Text.Encoding.Default.GetString(bytes);
+
+            findTask(id).UploadName = postedFile.FileName;
+            findTask(id).ContentType = postedFile.ContentType;
+            findTask(id).Data = bytes;
+
+            _db.SaveChanges();
+
+            return bytes;
+
+
+        }
+
+
+        //Download previously uploaded file
+
+        public FileContentResult downloadFile(int? FileId)
+        {
+            TaskListModels taskList = new TaskListModels();
+            var file = _db.TaskListModels.ToList().Find(p => p.ID == FileId);
+            return File(file.Data, file.ContentType, file.UploadName);
+        }
+
+        //Delete uploaded file
+
+        public TaskListModels deleteFile(int? FileId2)
+        {
+
+            var file = (from p in _db.TaskListModels where p.ID == FileId2 select p).FirstOrDefault();
+            file.UploadName = null;
+            file.ContentType = null;
+            file.Data = null;
+            _db.SaveChanges();
+            return file;
+        }
+
+
+
+
+    }
+}
